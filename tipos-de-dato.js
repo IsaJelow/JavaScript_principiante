8c06392cab8d95b206aeaaae3a36dd11f6351222
LAPTOP-8E52CDAU\IsaJelow <misajelow@gmail.com>
2025-03-04 20:15:07 -0600

    ejercicios tipos de datos

---
+++ tipos-de-dato.js
@@ -0,0 +1,33 @@
+console.log("Hola mundo"); //imprimimos en consola con developer tools
+//diferentes tipos de datos en javascript
+var nombre="isabel"; //string
+var edad= 24; //number
+var sueldo= 23.23;//number
+var casada=false;//bool
+var vacunas=true;//bool
+var hijos=null; //null
+var dato; //undefined
+
+console.log(typeof(nombre));
+console.log(typeof(edad));
+console.log(typeof(sueldo));
+console.log(typeof(casada));
+console.log(typeof(vacunas));
+console.log(typeof(hijos));
+console.log(typeof(dato));
+/*null dice que es object pero es ausencia de valor, es un error de incompatibilidad cpm javas q no se ha solucionado
+NaN not a Number, puede salir si quieres hacer operaciones con un numero y un no numero*/
+console.log(edad*nombre);
+
+//ejercicios de campus
+
+console.log("ejercicios de campus");
+console.log(typeof(42));
+console.log(typeof('Veinticinco'));
+console.log(typeof(-666));
+console.log(typeof(true));
+console.log(typeof(0));
+console.log(typeof('"'));
+console.log(typeof(null));
+console.log(typeof(FALSE));
+console.log(typeof(42));

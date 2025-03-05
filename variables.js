c2e92d2cef68ea418b65b4a22be1f95d86cea6b1
LAPTOP-8E52CDAU\IsaJelow <misajelow@gmail.com>
2025-03-04 20:30:24 -0600

    clase2 var,let and const

---
+++ variables.js
@@ -0,0 +1,13 @@
+//  var es la forma antigua de declarar variables, menos controladda
+var nombre="Ana";
+var nombre ="jose";
+
+//var no permite sobre escribir variable
+//scope de bloque
+let apellido = "gutierees";
+let apellido2="lopex";
+//permite cambiar valor, reasignar
+apellido="montes";
+
+//const es una constante, no se reasigna valor
+const PI = 3.1416;
